4c482280df90203a0510c6472cd4d2b66bcb9d3b
P-Grodecki <78574877+P-Grodecki@users.noreply.github.com>
2021-03-27 16:21:37 -0500

    SecondRevisions

--- app.py
+++ app.py
@@ -29,50 +29,43 @@ def import_from_csv():
         inventory_list = list(inventory_reader)
     for row in inventory_list:
         # ReAssign data from string to appropriate data types
-        row['product_price'] = round(float(row['product_price'].replace('$',''))*100,0)
-        row['product_quantity'] = int(row['product_quantity'])
-        row['date_updated'] = datetime.strptime(row['date_updated'], '%m/%d/%Y')
+        row = format_product_data_to_db(row['product_name'], row['product_price'], row['product_quantity'], row['date_updated'])
         write_to_db(row)
-        # try:
-        #     #Create new record in database
-        #     Product.create(**row)
-        # except IntegrityError:
-        #     # Find the other record with a matching name
-        #     matching_record = Product.get(product_name=row['product_name'])
-        #     # If the current row is more recent than existing record, then update record
-        #     if row['date_updated'] > matching_record.date_updated:
-        #         #print(matching_record.product_name)
-        #         #print(row['date_updated'].strftime('%m/%d/%Y') + " > " + matching_record.date_updated.strftime('%m/%d/%Y'))
-        #         matching_record.product_price = row['product_price']
-        #         matching_record.product_quantity = row['product_quantity']
-        #         matching_record.date_updated = row['date_updated']
-        #         matching_record.save()
 
-"""         # product_name,product_price,product_quantity,date_updated
-        print(row['product_name'] + " " + str(type(row['product_name'])) +
-            str(row['product_price']) + " " + str(type(row['product_price'])) +
-            str(row['product_quantity']) + " " + str(type(row['product_quantity'])) +
-            row['date_updated'].strftime('%m/%d/%Y') + " " + str(type(row['date_updated']))
-            ) """
+def format_product_data_to_db(arg_name, arg_price, arg_quantity, arg_date):
+    """Given the string name, decimal price, integer quantity, and '%m/%d/%Y' date, create formatted row dictionary."""
+    try:
+        data = {}
+        data['product_name'] = arg_name
+        data['product_price'] = round(float(arg_price.replace('$',''))*100,0)
+        data['product_quantity'] = int(arg_quantity)
+        data['date_updated'] = datetime.strptime(arg_date, '%m/%d/%Y')
+    except ValueError:
+        print('Oops! One of the entered values was not the correct data type.')
+        data = False
+    return data
 
 def write_to_db(row):
     """Given a dictionary of a row instance, write the row to the database.
     If a duplicate row is identified, then only update the record if new row was more recently updated."""
-    
     try:
-        #Create new record in database
+        # Create new record in database
         Product.create(**row)
+        # Return True to denote successful addition to DB
+        return True
     except IntegrityError:
         # Find the other record with a matching name
         matching_record = Product.get(product_name=row['product_name'])
         # If the current row is more recent than existing record, then update record
-        if row['date_updated'] > matching_record.date_updated:
+        if row['date_updated'] >= matching_record.date_updated:
             #print(matching_record.product_name)
             #print(row['date_updated'].strftime('%m/%d/%Y') + " > " + matching_record.date_updated.strftime('%m/%d/%Y'))
             matching_record.product_price = row['product_price']
             matching_record.product_quantity = row['product_quantity']
             matching_record.date_updated = row['date_updated']
             matching_record.save()
+            # Return string denoting that a row was updated.
+            return False
 
 
 def display_welcome_message(welcome_message):
@@ -111,37 +104,90 @@ def existing_product():
             #Exit this loop
             clear()
             break
-        try:
-            # Find the matching record and print the formatted details for each field.
-            matching_record = Product.get(product_id=user_id_choice)
-            print()
-            print(f'Product ID: {matching_record.product_id}')
-            print(f'Product Name: {matching_record.product_name}')
-            print('Product Price: ${:.2f}'.format(matching_record.product_price/100))
-            print(f'Quantity in Stock: {matching_record.product_quantity}')
-            print(f"Last Updated: {matching_record.date_updated.strftime('%m/%d/%Y')}")
-            print()
-            if input('Press any key to continue.').lower().strip() == 'b':
-                clear()
-                break
+        # try:
+        
+        # Find the matching record and print the formatted details for each field.
+        matching_record = Product.get(product_id=user_id_choice)
+        print()
+        format_product_data_from_db(matching_record, True)
+        print()
+        if input('Press any key to continue.').lower().strip() == 'b':
             clear()
-        except:
-            
-            clear()
-            print('Product with Entered ID does not exist. Try again.')
-
+            break
+        clear()
+        
+        # except:
+        #     clear()
+        #     print('Product with Entered ID does not exist. Try again.')
 
 def new_product():
     """Add a new product to the database."""
-    display_welcome_message("Add a new product to the database.")
-    input('Press any key to continue.')
+    display_welcome_message("Add a new product to the database."+
+                            "\n(Enter [b] to return to main menu.)")
+    print()
+    new_name = input('Product Name:     ')
+    new_price = input('Product Price:    $')
+    new_quantity = input('Product Quantity: ')
+    new_date = datetime.now().strftime('%m/%d/%Y')
+    the_formatted_data = format_product_data_to_db(new_name, new_price, new_quantity, new_date)
+    if the_formatted_data == False:
+        input('Product not added to inventory.\n' +
+                'Returning to Main Menu. Press any key to continue...')
+    else:
+        write_results = write_to_db(the_formatted_data)
+        if write_results == True:
+            input(f"Successfully added '{new_name}' to product inventory.\n" +
+                    'Returning to Main Meny. Press any key to continue')
+        elif write_results == False:
+            input(f"Product already exists, successfully updated '{new_name}' in product inventory.\n" +
+                    'Returning to Main Meny. Press any key to continue')
     clear()
 
 def backup_db():
     """Save a backup of the current product database."""
     display_welcome_message("Save a backup of the current product database.")
-    input('Press any key to continue.')
+    
+    with open('inventory_backup.csv', 'w') as csv_backup_file:
+        field_names = ['product_id',
+                        'product_name',
+                        'product_quantity',
+                        'product_price',
+                        'date_updated',
+        ]
+        write_backup = csv.DictWriter(csv_backup_file, field_names)
+        write_backup.writeheader()
+
+        all_records = Product.select()
+        for record in all_records:
+            record_dict = format_product_data_from_db(record, False)            
+            write_backup.writerow(record_dict)
+
+    input('File sucessfully backed up. Press any key to continue.')
     clear()
+
+def format_product_data_from_db(the_record, to_print):
+    """Given A single record from teh inventory database,
+    format the various fields into strings stored in a dictionary.
+    Print the formatted fields in user-friendly format.
+    Return dictionary of formatted string values."""
+
+    formatted_record = OrderedDict([
+        ('product_id', the_record.product_id),
+        ('product_name', the_record.product_name),
+        ('product_price', '${:.2f}'.format(the_record.product_price/100)), 
+        ('product_quantity', the_record.product_quantity),
+        ('date_updated', the_record.date_updated.strftime('%m/%d/%Y')),
+    ])
+    
+    if to_print:
+        print('Product ID: {}'.format(formatted_record['product_id']))
+        print('Product Name: {}'.format(formatted_record['product_name']))
+        print('Product Price: {}'.format(formatted_record['product_price']))
+        print('Quantity in Stock: {}'.format(formatted_record['product_qunatity']))
+        print('Last Updated: {}'.format(formatted_record['date_updated']))
+    
+    return formatted_record
+
 
 def clear():
     os.system('cls' if os.name == 'nt' else clear)
